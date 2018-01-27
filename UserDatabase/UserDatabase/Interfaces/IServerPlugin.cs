8d4b65a819a35f33724edf403ac6712ef2a0560c
Alexander Ritter <braydelritter@gmail.com>
2018-01-27 16:30:39 -0600

    First debug entry

---
+++ UserDatabase/UserDatabase/Interfaces/IServerPlugin.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Sockets;
+
+namespace UniversalServer.Interfaces
+{
+    public interface IServerPlugin
+    {
+        string Name { get; }
+        void Invoke(TcpListener _serverSocket, TcpClient _clientSocket, int port, string UserRequest, string ServerDirectory);
+        void onLoad(string ServerDirectory);
+    }
+}
---
+++ UserDatabase/UserDatabase/Main.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Sockets;
+using UniversalServer.Interfaces;
+
+namespace vortexstudio.universalserver.userdatabase
+{
+    public class UserDatabase : IServerPlugin
+    {
+        public string Name { get { return "userdatabase"; } }
+
+        #region OnLoad
+
+        //Activates when the server loads the plugin
+        public void onLoad(string ServerDirectory)
+        {
+            
+        }
+
+        #endregion
+
+        #region OnInvoke
+
+        //Activates when the plugin has been invoked
+        public void Invoke(TcpListener _serverSocket, TcpClient _clientSocket, int port, string UserRequest, string ServerDirectory)
+        {
+            Console.WriteLine(UserRequest);
+            Console.WriteLine(ServerDirectory);
+        }
+
+        #endregion
+    }
+}
